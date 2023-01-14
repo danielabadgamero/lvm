c67d7114b575127ba6c92790756404afd875c139
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-01-14 20:35:04 +0100

    Add text input.

--- LightningCore.cpp
+++ LightningCore.cpp
@@ -1,22 +1,39 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <SDL_ttf.h>
 
 #include "LightningCore.h"
 #include "LightningCPU.h"
-#include "LightningVGA.h"
 
 void Lightning::init(SDL_Point screenSize)
 {
 	windowSize = screenSize;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
+	IMG_Init(IMG_INIT_PNG);
+	TTF_Init();
 
 	window = SDL_CreateWindow("Lightning VM", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowSize.x, windowSize.y, SDL_WINDOW_BORDERLESS);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	VGA::thread = SDL_CreateThread(VGA::cycle, "VGA", NULL);
 	CPU::thread = SDL_CreateThread(CPU::cycle, "CPU", NULL);
 
+	TTF_Font* font{ TTF_OpenFont("lucon.ttf", height) };
+
+	TTF_GlyphMetrics(font, 'a', NULL, NULL, NULL, NULL, &advance);
+
+	for (int i{}; i != 127; i++)
+	{
+		SDL_Surface* surface{ TTF_RenderGlyph32_Blended(font, static_cast<char>(i), {0xff, 0xff, 0xff}) };
+		glyphs[i] = SDL_CreateTextureFromSurface(renderer, surface);
+		SDL_FreeSurface(surface);
+	}
+
+	glyphs[127] = IMG_LoadTexture(renderer, "cursor.png");
+
+	cursorPos = VIDEO;
+
+	TTF_CloseFont(font);
 	SDL_ShowCursor(SDL_DISABLE);
 	running = true;
 }
@@ -32,22 +49,47 @@ void Lightning::cycle()
 			case SDL_SCANCODE_ESCAPE:
 				running = false;
 				break;
+			case SDL_SCANCODE_BACKSPACE:
+				RAM[cursorPos] = 0;
+				if (cursorPos > 0)
+					cursorPos--;
+				break;
+			case SDL_SCANCODE_LSHIFT:
+			case SDL_SCANCODE_RSHIFT:
+			case SDL_SCANCODE_LCTRL:
+			case SDL_SCANCODE_RCTRL:
+			case SDL_SCANCODE_LALT:
+			case SDL_SCANCODE_RALT:
+				break;
+			default:
+				RAM[cursorPos] = static_cast<char>(e.key.keysym.sym);
+				if (SDL_GetModState() & (KMOD_LSHIFT | KMOD_RSHIFT))
+					RAM[cursorPos] -= 0x20;
+				cursorPos++;
+				break;
 			}
 			break;
 		}
 
+	RAM[cursorPos] = 127;
+
 	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
 	SDL_RenderClear(renderer);
 
-	SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
-	SDL_RenderDrawPoints(renderer, screen, WIDTH * HEIGHT);
+	for (int i{}; i != (windowSize.x / advance) * (windowSize.y / height); i++)
+	{
+		SDL_Rect glyph{ ((i * advance) % windowSize.x), ((i * advance) / windowSize.x) * height, advance, height };
+		SDL_RenderCopy(renderer, glyphs[RAM[VIDEO + i]], NULL, &glyph);
+	}
 
 	SDL_RenderPresent(renderer);
 }
 
 void Lightning::quit()
 {
-	SDL_WaitThread(VGA::thread, NULL);
+	for (int i{}; i != 128; i++)
+		SDL_DestroyTexture(glyphs[i]);
+
 	SDL_WaitThread(CPU::thread, NULL);
 
 	SDL_DestroyWindow(window);
--- LightningCore.h
+++ LightningCore.h
@@ -12,10 +12,13 @@ namespace Lightning
 	inline SDL_Event e{};
 	inline SDL_Point windowSize{};
 	inline SDL_Texture* glyphs[128]{};
+	inline int advance{};
+	inline constexpr int height{ 15 };
+	inline int cursorPos{};
 
 	inline bool running{};
 
-	inline int RAM[1 << 20]{};
+	inline char RAM[1 << 20]{};
 
 	void init(SDL_Point);
 	void cycle();
