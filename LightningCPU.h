1c7669e09309ab2bf3bd0c31ecd071404e5243ba
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-01-20 18:03:14 +0100

    Reduced registers to 4.

--- LightningCPU.h
+++ LightningCPU.h
@@ -13,21 +13,9 @@ namespace Lightning::CPU
 		MOV,	// MOV	000 11111111
 	};
 
-	enum Reg
-	{
-		r1,	// general purpose regiser 1
-		r2,	// general purpose regiser 2
-		r3,	// general purpose regiser 3
-		r4,	// general purpose regiser 4
-		  
-		ar,	// address register
-		dr,	// data register
-		  
-		ir,	// instruction register
-		pc,	// programme counter register
-	};
+	inline int pc{};
 
-	inline int reg[8]{};
+	inline int reg[4]{};
 
 	inline int stack[1 << 16]{}; // 64 KB
 	inline int sb{};
