083ec25ca189f4e3c09c9c173fc1b2d9965c017a
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-03-29 20:18:28 +0200

    Adapt for 32-bit architecture.

--- LightningCPU.cpp
+++ LightningCPU.cpp
@@ -2,149 +2,26 @@
 #include "LightningCPU.h"
 
 #define opcode ir.bitfields.opcode
-#define rDest reg[ir.bitfields.reg]
-#define rSource reg[ir.bitfields.op2]
-#define imm8 static_cast<unsigned char>(ir.bitfields.op2)
-#define imm10 static_cast<unsigned short>(ir.bitfields.reg << 8) + ir.bitfields.op2
-#define aMode ir.bitfields.addrMode
-#define value ((aMode == 0) ? rSource : imm8)
-#define value10 ((aMode == 0) ? rSource : imm10)
+#define address ir.bitfields.address
+#define operand ir.bitfields.operand
 
-static void readMemory(short address, short* dest)
+static void readMemory(int addr, int* dest)
 {
 	if (Lightning::Core::chipSelected)
-		*dest = Lightning::Core::RAM[address];
+		*dest = Lightning::Core::RAM[addr];
 	else
-		*dest = Lightning::Core::ROM[address];
+		*dest = Lightning::Core::ROM[addr];
 }
 
-static void writeMemory(short address, short data)
+static void writeMemory(int addr, int data)
 {
-	Lightning::Core::RAM[address] = static_cast<unsigned char>(data);
+	Lightning::Core::RAM[addr] = static_cast<unsigned char>(data);
 }
 
 void Lightning::CPU::decode()
 {
 	switch (opcode)
 	{
-		//	Data management
-	case UNUSED_1:
-		break;
-	case MOV:
-		rDest = value;
-		break;
-	case SPB:
-		pb = value;
-		break;
-	case PSHB:
-		stack.push(pb);
-		break;
-	case POPB:
-		pb = stack.top();
-		stack.pop();
-		break;
-	case LD:
-		readMemory(value + pb, &rDest);
-		break;
-	case ST:
-		writeMemory(rDest, value + pb);
-		break;
-	case PUSH:
-		stack.push(value);
-		break;
-	case POP:
-		rDest = stack.top();
-		stack.pop();
-		break;
-	case IN:
-		break;
-	case OUT:
-		break;
-		
-		//	Flow control
-	case HALT:
-		if (aMode == 0)
-			Core::running = false;
-		else
-			Core::chipSelected = imm8;
-		pc = 0;
-		break;
-	case CMP:
-		compFlags[equal] = rDest == value;
-		compFlags[greater] = rDest > value;
-		compFlags[greater_equal] = rDest >= value;
-		compFlags[less] = rDest < value;
-		compFlags[less_equal] = rDest <= value;
-		break;
-	case JMP:
-		pc = value10 + pb;
-		break;
-	case JEQ:
-		if (compFlags[equal])
-			pc = value10 + pb;
-		break;
-	case JNE:
-		if (compFlags[not_equal])
-			pc = value10 + pb;
-		break;
-	case JGT:
-		if (compFlags[greater])
-			pc = value10 + pb;
-		break;
-	case JGE:
-		if (compFlags[greater_equal])
-			pc = value10 + pb;
-		break;
-	case JLT:
-		if (compFlags[less])
-			pc = value10 + pb;
-		break;
-	case JLE:
-		if (compFlags[less_equal])
-			pc = value10 + pb;
-		break;
-	case CALL:
-		stack.push(pc);
-		pc = value10 + pb;
-		break;
-	case RET:
-		pc = stack.top();
-		stack.pop();
-		break;
-
-		//	Arithmetic
-	case ADD:
-		rDest += value;
-		break;
-	case SUB:
-		rDest -= value;
-		break;
-	case MUL:
-		rDest *= value;
-		break;
-	case DIV:
-		rDest /= value;
-		break;
-	case MOD:
-		rDest %= value;
-		break;
-	case SHFT:
-		rDest <<= value;
-		break;
-
-		//	Logic
-	case AND:
-		rDest &= value;
-		break;
-	case OR:
-		rDest |= value;
-		break;
-	case XOR:
-		rDest ^= value;
-		break;
-	case NOT:
-		rDest = ~value;
-		break;
 	}
 }
 
@@ -154,13 +31,13 @@ int Lightning::CPU::cycle(void*)
 
 	while (Core::running)
 	{
-		short instruction{};
-		for (int i{}; i != 2; i++)
+		int instruction{};
+		for (int i{}; i != 4; i++)
 		{
-			readMemory(pc, &instruction);
+			readMemory(regs[pc], &instruction);
 			ir.instruction <<= 8;
 			ir.instruction += instruction;
-			pc++;
+			regs[pc]++;
 		}
 		decode();
 	}
--- LightningCPU.h
+++ LightningCPU.h
@@ -5,82 +5,35 @@
 
 namespace Lightning::CPU
 {
-	enum CompFlag
-	{
-		equal,
-		not_equal,
-		greater,
-		greater_equal,
-		less,
-		less_equal,
-
-		total_compFlags
-	};
-
 	enum Opcode
 	{
-		//			Description (addrMode == 0)			Description (addrMode == 1)
-		UNUSED_1,
 
-		//	Data management
-		MOV,	//	Load reg op2 to rDest				Load imm op2 to rDest
-		SPB,	//	Set programme base to reg op2		Set programme base to imm op2
-		PSHB,	//	Push programme base to stack		Push programme base to stack
-		POPB,	//	Pop programme base from stack		Pop programme base from stack
-		LD,		//	Load reg addr op2 to rDest			Load addr op2 to rDest
-		ST,		//	Store reg op2 in addr rDest			Store imm op2 in addr rDest
-		PUSH,	//	Push reg op2 to stack				Push imm op2 to stack
-		POP,	//	Pop top of stack into rDest			Pop top of stack into rDest
-		IN,		//	TBD: communication with peripherals.
-		OUT,	//	TBD: communication with peripherals.
-
-		//	Flow control
-		HALT,	//	Finish execution					Switch to chip in op2
-		CMP,	//	Eval rDest and reg op2 to flags		Eval rDest and imm op2 to flags
-		JMP,	//	Set pc to reg op2					Set pc to imm op2
-		JEQ,	//	Set pc to reg op2 if equal flag		Set pc to imm op2 if equal flag
-		JNE,	//	Set pc to reg op2 if not equal flag	Set pc to imm op2 if not equal flag
-		JGT,	//	Set pc to reg op2 if greater flag	Set pc to imm op2 if greater flag
-		JGE,	//	Set pc to reg op2 if grt eq flag	Set pc to imm op2 if grt eq flag
-		JLT,	//	Set pc to reg op2 if less flag		Set pc to imm op2 if less flag
-		JLE,	//	Set pc to reg op2 if less eq flag	Set pc to imm op2 if less eq flag
-		CALL,	//	Push pc to stack and set to reg op2	Push pc to stack and set to imm op2
-		RET,	//	Pop top of stack and jump to it		Pop top of stack and jump to it
-
-		//	Arithmetic
-		ADD,	//	Add reg op2 to rDest				Add imm op2 to rDest
-		SUB,	//	Substract reg op2 from rDest		Substract imm op2 from rDest
-		MUL,	//	Multiply reg op2 times rDest		Multiply imm op2 times rDest
-		DIV,	//	Divide reg op2 by rDest				Divide imm op2 by rDest
-		MOD,	//	Modulo of rDest by reg op2			Modulo of rDest by imm op2
-		SHFT,	//	Shift rDest to the left by reg op2	Shift rDest to the left by imm op2
-
-		//	Logic
-		AND,	//	Bitwise AND of rDest and reg op2	Bitwise AND of rDest and imm op2
-		OR,		//	Bitwise OR of rDest and reg op2		Bitwise OR of rDest and imm op2
-		XOR,	//	Bitwise XOR of rDest and reg op2	Bitwise XOR of rDest and imm op2
-		NOT,	//	Bitwise neg of reg op2 in rDest		Bitwise neg of imm op2 in rDest
 	};
-
-	inline short reg[4]{};
-
-	inline bool compFlags[total_compFlags]{};
 
 	inline union
 	{
 		struct
 		{
-			unsigned short op2 : 8;			// Second operand. Interpretation depends on adrMode
-			unsigned short reg : 2;			// 4 general purpose registers
-			unsigned short addrMode : 1;	// Opcode-specific addressing mode modifier
+			unsigned short operand : 24;	// 24-bit immediate
+			unsigned short address : 3;		// Address specifier
 			unsigned short opcode : 5;		// 32 different opcodes
 		} bitfields;
 
 		unsigned short instruction{};
 	} ir{};
 
-	inline unsigned short pc{};
-	inline unsigned short pb{};
+	enum Reg
+	{
+		ax = 1,
+		bx,
+		cx,
+		dx,
+		pc,
+		pb,
+		tr,
+	};
+
+	inline int regs[7]{};
 
 	inline std::stack<short> stack{};
 
