a5a3225397d4a42ff0845913605f7750f130428d
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-14 20:56:33 +0200

    Add opcode string vector.

--- Assembler.cpp
+++ Assembler.cpp
@@ -3,7 +3,31 @@
 #include <unordered_map>
 
 #include "Assembler.h"
-#include "Lightning.h"
+
+std::vector<std::string> opcodes
+{
+	// Data management
+	"MOV",
+	"PUSH",
+	"POP",
+	"RD",
+	"WR",
+	"REC",
+
+	// Flow control
+	"HALT",
+	"JMP",
+	"CALL",
+	"RET",
+
+	// Arithmetic and bitwise logic
+	"ADD",
+	"MUL",
+	"DIV",
+	"CMP",
+	"AND",
+	"NOT"
+};
 
 std::vector<char> Assembler::assemble(std::vector<std::string>& input)
 {
@@ -20,10 +44,11 @@ std::vector<char> Assembler::assemble(std::vector<std::string>& input)
 		std::vector<std::string> words(1);
 		bool quotes{};
 		for (const char& c : line)
-			if (c == '\t') continue;
-			else if (c == ' ' && !words.back().empty() && !quotes) words.push_back("");
+			if ((c == ' ' || c == '\t') && !words.back().empty() && !quotes) words.push_back("");
 			else if (c == '"') quotes ^= 1;
-			else words.back().push_back(c);
+			else if (c != ',') words.back().push_back(c);
+
+		std::vector<std::string>::iterator opcodeIt{ std::find(opcodes.begin(), opcodes.end(), words[0]) };
 	}
 	
 	return bin;
