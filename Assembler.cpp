bec8b90e60df986d652f8d282a6d8fea13bbdbfe
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-15 07:45:38 +0200

    Add assembler stuff.

--- Assembler.cpp
+++ Assembler.cpp
@@ -29,6 +29,22 @@ std::vector<std::string> opcodes
 	"NOT"
 };
 
+static constexpr bool regChar(const char& c)
+{
+	return ((c >= 'a' && c <= 'd') || c == 'l' || c == 'h');
+}
+
+static unsigned char getRegByte(std::string reg)
+{
+	unsigned char regByte{ ((reg[0] - 'a') << 6) | 0b00'111111 };
+	for (int i{ 1 }; i != reg.size(); i++)
+		if (reg[i] == 'h')
+			regByte ^= (0b01'00'00'00 >> (2 * i));
+		else regByte ^= (0b10'00'00'00 >> (2 * i));
+
+	return regByte;
+}
+
 std::vector<char> Assembler::assemble(std::vector<std::string>& input)
 {
 	std::vector<char> bin{};
@@ -85,6 +101,53 @@ std::vector<char> Assembler::assemble(std::vector<std::string>& input)
 		else
 		{
 			unsigned char opcode{ static_cast<unsigned char>(std::distance(opcodes.begin(), opcodeIt)) << 4 };
+			unsigned char dest{};
+			unsigned char ddMode{};
+			unsigned char src{};
+			unsigned char sdMode{};
+
+			switch (words.size())
+			{
+			case 3:
+				if (words[1][0] == '[')
+					(ddMode = 1 << 2), words[1].erase(words[1].begin()), words[1].pop_back();
+				bool reg{ true };
+				for (const char& c : words[1])
+					if (!regChar(c)) reg = false;
+				if (!reg)
+				{
+					dest = 1 << 3;
+					try
+					{
+						short n{ std::stoi(words[1], NULL, 16) };
+						bin.push_back((n & 0xff00) >> 8);
+						bin.push_back(static_cast<char>(n));
+						pc += 2;
+					}
+					catch (...)
+					{
+						labelRef.emplace(words[1], pc);
+						bin.push_back(0);
+						bin.push_back(0);
+						pc += 2;
+					}
+				}
+				else
+					bin.push_back(getRegByte(words[1]));
+			case 2:
+				if (words[words.size() - 1][0] == '[')
+					(sdMode = 1), words[1].erase(words[words.size() - 1].begin()), words[words.size() - 1].pop_back();
+				reg = true;
+				for (const char& c : words[words.size() - 1])
+					if (!regChar(c)) reg = false;
+				if (!reg)
+				{
+					src = 1 << 1;
+				}
+				else
+					bin.push_back(getRegByte(words[words.size() - 1]));
+				break;
+			}
 		}
 	}
 	
